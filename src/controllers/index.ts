be8eca33ef201b8fbb293e05bc84c9638ff357e8
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-07-13 21:29:09 +0100

    create: create server

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,1 @@
-export * from './greeting.controller';
+
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
+import fs from 'fs';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+const file = fs.readFileSync(path.join(__dirname, "openapi.yaml"), "utf8");
+const swaggerDocument = YAML.parse(file);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Swagger UI at available at http://localhost:${port}/api-docs`);
+});
+
+export default app;
